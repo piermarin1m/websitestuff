3f680cb6bfb2b7069c7d0d9e88a41503199e225f
piermarin1m <159067706+piermarin1m@users.noreply.github.com>
2024-11-21 01:00:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
     layout="wide"
 )
 
-BACKEND_URL = "https://hidden-castle-92662-99aea616550d.herokuapp.com"
+BACKEND_URL = "http://150.136.82.157:1692"
 TIMEOUT_SECONDS = 30
 
 # Display the Schoology title
